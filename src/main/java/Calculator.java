0f479bdde49cb3178bf4a375801f2a121481af09
Ilya-Karasev <uvp112.karasev.i@mail.ru>
2023-12-06 22:27:29 +0300

    Karasev Ilya Sergeevich

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,27 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("Невозможно делить на 0");
+        }
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    public double solver(double a, double b) {
+        if (a != 0) {
+            return -b / a;
+        } else {
+            throw new IllegalArgumentException("Уравнение не является линейным");
+        }
     }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,48 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Calculator calculator = new Calculator();
+        int result = calculator.add(3, 5);
+        assertEquals(8, result);
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Calculator calculator = new Calculator();
+        int result = calculator.dif(8, 3);
+        assertEquals(5, result);
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Calculator calculator = new Calculator();
+        int result = calculator.div(10, 2);
+        assertEquals(5, result);
+
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.div(5, 0); // division by zero should throw ArithmeticException
+        });
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Calculator calculator = new Calculator();
+        int result = calculator.times(4, 6);
+        assertEquals(24, result);
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calculator = new Calculator();
+
+        // Тест для линейного уравнения ax + b = 0
+        double a = 2;
+        double b = -6;
+        double result = calculator.solver(a, b);
+        assertEquals(3.0, result, 0.0001);  // Проверяем, что результат равен 3 с погрешностью 0.0001
     }
 }
